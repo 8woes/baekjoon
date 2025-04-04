1cd2a22229bf50f9a52155df7a0ba9fd59fb43d1
chaeminhwa <chaeminhwa@localhost>
2025-04-04 09:48:13 +0900

    q2525

---
+++ Baekjoon/src/q2525/Main.java
@@ -0,0 +1,25 @@
+package q2525;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		sc.close();
+		
+		int D = B + C;
+		if (D >= 60) {
+			int d = D / 60;
+			D -= (60 * d);
+			A += d;
+			if (A >= 24) {
+				int a = A / 24;
+				A -= (24 * a);	
+			}
+		}
+		System.out.println(A + " " + D);
+	}
+}
